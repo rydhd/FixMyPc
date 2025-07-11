67be579f3bd7f350431fa3c10ba127e2873021e8
rydhd <saracanlao72@gmail.com>
2025-07-11 13:57:21 +0800

    login and register fix the template

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,68 +4,79 @@
 
 <?= $this->section('main') ?>
 
-    <div id="auth-left">
-        <div class="auth-logo">
-            <a href="/"><img src="<?= base_url('assets/compiled/svg/logo.svg') ?>" alt="Logo"></a>
-        </div>
-        <h1 class="auth-title"><?= lang('Auth.login') ?></h1>
-        <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
+    <div id="auth">
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="/"><img src="<?= base_url('assets/compiled/svg/logo.svg') ?>" alt="Logo"></a>
+                    </div>
+                    <h1 class="auth-title"><?= lang('Auth.login') ?></h1>
+                    <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
 
-        <?php if (session('message') !== null) : ?>
-            <div class="alert alert-success" role="alert"><?= session('message') ?></div>
-        <?php endif ?>
+                    <?php if (session('message') !== null) : ?>
+                        <div class="alert alert-success" role="alert"><?= session('message') ?></div>
+                    <?php endif ?>
 
-        <?php if (session('error') !== null) : ?>
-            <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
-        <?php elseif (session('errors') !== null) : ?>
-            <div class="alert alert-danger" role="alert">
-                <?php if (is_array(session('errors'))) : ?>
-                    <?php foreach (session('errors') as $error) : ?>
-                        <?= $error ?>
-                        <br>
-                    <?php endforeach ?>
-                <?php else : ?>
-                    <?= session('errors') ?>
-                <?php endif ?>
-            </div>
-        <?php endif ?>
+                    <?php if (session('error') !== null) : ?>
+                        <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+                    <?php elseif (session('errors') !== null) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?php if (is_array(session('errors'))) : ?>
+                                <?php foreach (session('errors') as $error) : ?>
+                                    <?= $error ?>
+                                    <br>
+                                <?php endforeach ?>
+                            <?php else : ?>
+                                <?= session('errors') ?>
+                            <?php endif ?>
+                        </div>
+                    <?php endif ?>
 
+                    <form action="<?= url_to('login') ?>" method="post">
+                        <?= csrf_field() ?>
 
-        <form action="<?= url_to('login') ?>" method="post">
-            <?= csrf_field() ?>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="email" class="form-control form-control-xl" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required>
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
 
-            <div class="form-group position-relative has-icon-left mb-4">
-                <input type="email" class="form-control form-control-xl" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required>
-                <div class="form-control-icon">
-                    <i class="bi bi-person"></i>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" name="password" inputmode="text" autocomplete="current-password" placeholder="<?= lang('Auth.password') ?>" required>
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+
+                        <?php if (setting('Auth.sessionConfig')['allowRemembering']): ?>
+                            <div class="form-check form-check-lg d-flex align-items-end">
+                                <input class="form-check-input me-2" type="checkbox" name="remember" <?php if (old('remember')): ?> checked<?php endif ?>>
+                                <label class="form-check-label text-gray-600" for="flexCheckDefault">
+                                    <?= lang('Auth.rememberMe') ?>
+                                </label>
+                            </div>
+                        <?php endif; ?>
+
+                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5"><?= lang('Auth.login') ?></button>
+                    </form>
+
+                    <div class="text-center mt-5 text-lg fs-4">
+                        <?php if (setting('Auth.allowRegistration')) : ?>
+                            <p class="text-gray-600"><?= lang('Auth.needAccount') ?> <a href="<?= url_to('register') ?>" class="font-bold"><?= lang('Auth.register') ?></a></p>
+                        <?php endif ?>
+                        <?php if (setting('Auth.allowMagicLinkLogins')) : ?>
+                            <p><a class="font-bold" href="<?= url_to('magic-link') ?>"><?= lang('Auth.forgotPassword') ?></a></p>
+                        <?php endif ?>
+                    </div>
                 </div>
             </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
 
-            <div class="form-group position-relative has-icon-left mb-4">
-                <input type="password" class="form-control form-control-xl" name="password" inputmode="text" autocomplete="current-password" placeholder="<?= lang('Auth.password') ?>" required>
-                <div class="form-control-icon">
-                    <i class="bi bi-shield-lock"></i>
                 </div>
             </div>
-
-            <?php if (setting('Auth.sessionConfig')['allowRemembering']): ?>
-                <div class="form-check form-check-lg d-flex align-items-end">
-                    <input class="form-check-input me-2" type="checkbox" name="remember" <?php if (old('remember')): ?> checked<?php endif ?>>
-                    <label class="form-check-label text-gray-600" for="flexCheckDefault">
-                        <?= lang('Auth.rememberMe') ?>
-                    </label>
-                </div>
-            <?php endif; ?>
-
-            <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5"><?= lang('Auth.login') ?></button>
-        </form>
-        <div class="text-center mt-5 text-lg fs-4">
-            <?php if (setting('Auth.allowRegistration')) : ?>
-                <p class="text-gray-600"><?= lang('Auth.needAccount') ?> <a href="<?= url_to('register') ?>" class="font-bold"><?= lang('Auth.register') ?></a></p>
-            <?php endif ?>
-            <?php if (setting('Auth.allowMagicLinkLogins')) : ?>
-                <p><a class="font-bold" href="<?= url_to('magic-link') ?>"><?= lang('Auth.forgotPassword') ?></a></p>
-            <?php endif ?>
         </div>
     </div>
 
--- app/Views/register.php
+++ app/Views/register.php
@@ -4,61 +4,81 @@
 
 <?= $this->section('main') ?>
 
-    <div class="container d-flex justify-content-center p-5">
-        <div class="card col-12 col-md-5 shadow-sm">
-            <div class="card-body">
-                <h5 class="card-title mb-5"><?= lang('Auth.register') ?></h5>
-
-                <?php if (session('error') !== null) : ?>
-                    <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
-                <?php elseif (session('errors') !== null) : ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?php if (is_array(session('errors'))) : ?>
-                            <?php foreach (session('errors') as $error) : ?>
-                                <?= $error ?>
-                                <br>
-                            <?php endforeach ?>
-                        <?php else : ?>
-                            <?= session('errors') ?>
-                        <?php endif ?>
+    <div id="auth">
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="/"><img src="./assets/compiled/svg/logo.svg" alt="Logo"></a>
                     </div>
-                <?php endif ?>
+                    <h1 class="auth-title"><?= lang('Auth.register') ?></h1>
+                    <p class="auth-subtitle mb-5">Input your data to register to our website.</p>
 
-                <form action="<?= url_to('register') ?>" method="post">
-                    <?= csrf_field() ?>
+                    <?php if (session('error') !== null) : ?>
+                        <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+                    <?php elseif (session('errors') !== null) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?php if (is_array(session('errors'))) : ?>
+                                <?php foreach (session('errors') as $error) : ?>
+                                    <?= $error ?>
+                                    <br>
+                                <?php endforeach ?>
+                            <?php else : ?>
+                                <?= session('errors') ?>
+                            <?php endif ?>
+                        </div>
+                    <?php endif ?>
 
-                    <div class="form-floating mb-2">
-                        <input type="text" class="form-control" id="floatingAccessCodeInput" name="access_code" placeholder="Access Code" value="<?= old('access_code') ?>" required>
-                        <label for="floatingAccessCodeInput">Access Code</label>
+                    <form action="<?= url_to('register') ?>" method="post">
+                        <?= csrf_field() ?>
+
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" class="form-control form-control-xl" name="access_code" placeholder="Access Code" value="<?= old('access_code') ?>" required>
+                            <div class="form-control-icon">
+                                <i class="bi bi-key"></i>
+                            </div>
+                        </div>
+
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="email" class="form-control form-control-xl" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required>
+                            <div class="form-control-icon">
+                                <i class="bi bi-envelope"></i>
+                            </div>
+                        </div>
+
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" class="form-control form-control-xl" name="username" inputmode="text" autocomplete="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>" required>
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" name="password" inputmode="text" autocomplete="new-password" placeholder="<?= lang('Auth.password') ?>" required>
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" name="password_confirm" inputmode="text" autocomplete="new-password" placeholder="<?= lang('Auth.passwordConfirm') ?>" required>
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5"><?= lang('Auth.register') ?></button>
+                    </form>
+
+                    <div class="text-center mt-5 text-lg fs-4">
+                        <p class='text-gray-600'><?= lang('Auth.haveAccount') ?> <a href="<?= url_to('login') ?>" class="font-bold"><?= lang('Auth.login') ?></a></p>
                     </div>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
 
-                    <div class="form-floating mb-2">
-                        <input type="email" class="form-control" id="floatingEmailInput" name="email" inputmode="email" autocomplete="email" placeholder="<?= lang('Auth.email') ?>" value="<?= old('email') ?>" required>
-                        <label for="floatingEmailInput"><?= lang('Auth.email') ?></label>
-                    </div>
-
-                    <div class="form-floating mb-4">
-                        <input type="text" class="form-control" id="floatingUsernameInput" name="username" inputmode="text" autocomplete="username" placeholder="<?= lang('Auth.username') ?>" value="<?= old('username') ?>" required>
-                        <label for="floatingUsernameInput"><?= lang('Auth.username') ?></label>
-                    </div>
-
-                    <div class="form-floating mb-2">
-                        <input type="password" class="form-control" id="floatingPasswordInput" name="password" inputmode="text" autocomplete="new-password" placeholder="<?= lang('Auth.password') ?>" required>
-                        <label for="floatingPasswordInput"><?= lang('Auth.password') ?></label>
-                    </div>
-
-                    <div class="form-floating mb-5">
-                        <input type="password" class="form-control" id="floatingPasswordConfirmInput" name="password_confirm" inputmode="text" autocomplete="new-password" placeholder="<?= lang('Auth.passwordConfirm') ?>" required>
-                        <label for="floatingPasswordConfirmInput"><?= lang('Auth.passwordConfirm') ?></label>
-                    </div>
-
-                    <div class="d-grid col-12 col-md-8 mx-auto m-3">
-                        <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.register') ?></button>
-                    </div>
-
-                    <p class="text-center"><?= lang('Auth.haveAccount') ?> <a href="<?= url_to('login') ?>"><?= lang('Auth.login') ?></a></p>
-
-                </form>
+                </div>
             </div>
         </div>
     </div>
